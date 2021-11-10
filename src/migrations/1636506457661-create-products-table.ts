9526c2c2e1e0d8f96983cd34511e8eb81d01cff1
enzobonf <enzobonfx@gmail.com>
2021-11-09 23:03:11 -0300

    products services, typeorm & docker

---
+++ src/migrations/1636506457661-create-products-table.ts
@@ -0,0 +1,29 @@
+import { MigrationInterface, QueryRunner, Table } from 'typeorm';
+
+export class CreateProductsTable1636506457661 implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.createTable(
+      new Table({
+        name: 'products',
+        columns: [
+          {
+            name: 'id',
+            type: 'int',
+            isGenerated: true,
+            generationStrategy: 'increment',
+          },
+          {
+            name: 'name',
+            type: 'varchar',
+          },
+          {
+            name: 'price',
+            type: 'double precision',
+          },
+        ],
+      }),
+    );
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {}
+}
